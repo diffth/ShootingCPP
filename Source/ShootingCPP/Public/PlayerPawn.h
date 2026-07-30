220453304e12e55a17ebf82b813d7d811dca7c9b
diffth <diffth@nate.com>
2026-07-30 22:10:58 +0900

    Add MoveHorizontal declaration to PlayerPawn

--- Source/ShootingCPP/Public/PlayerPawn.h
+++ Source/ShootingCPP/Public/PlayerPawn.h
@@ -49,6 +49,8 @@ private:
 	float h;
 	float v;
 	
+	void MoveHorizontal(float value);
+	
 	// Enhanced Input 액션 이벤트 콜백 함수
 	void OnInputHorizontal(const struct FInputActionValue& value);
 	void OnInputVertical(const struct FInputActionValue& value);
