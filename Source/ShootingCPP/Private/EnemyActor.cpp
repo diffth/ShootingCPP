619ea2b5c69cd21b492f4c73fd75bf6b359f33b2
diffth <diffth@nate.com>
2026-08-01 12:11:45 +0900

    0801 04

--- Source/ShootingCPP/Private/EnemyActor.cpp
+++ Source/ShootingCPP/Private/EnemyActor.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "EnemyActor.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 AEnemyActor::AEnemyActor()
@@ -9,6 +10,12 @@ AEnemyActor::AEnemyActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collider"));
+	SetRootComponent(boxComp);
+	boxComp->SetBoxExtent(FVector(50.0f, 50.0f, 50.0f));
+	
+	meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static mesh"));
+	meshComp->SetupAttachment(boxComp);
 }
 
 // Called when the game starts or when spawned
