ce56897041ef2d0be543bf1c2b172ec0a82a815f
diffth <diffth@nate.com>
2026-08-03 20:09:43 +0900

    0803 07

--- Source/ShootingCPP/Public/Bullet.h
+++ Source/ShootingCPP/Public/Bullet.h
@@ -37,4 +37,7 @@ public:
 	
 	// UPROPERTY(EditAnywhere)
 	// float moveSpeed = 1000.0f;
+	
+	UFUNCTION()
+	void OnBulletOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
